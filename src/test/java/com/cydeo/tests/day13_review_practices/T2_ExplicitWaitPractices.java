f7b9520672febfbed86b2588b67e6cffdf92a8c5
NaziraEU8 <nazirakrt@gmail.com>
2023-02-24 17:01:29 +0100

    Task 2.ExplicitWaitPractices.java done

--- src/test/java/com/cydeo/tests/day13_review_practices/T2_ExplicitWaitPractices.java
+++ src/test/java/com/cydeo/tests/day13_review_practices/T2_ExplicitWaitPractices.java
@@ -1,10 +1,9 @@
 package com.cydeo.tests.day13_review_practices;
 
 import com.cydeo.pages.DynamicControlsPage;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -26,15 +25,18 @@ public class T2_ExplicitWaitPractices {
      dynamicControlsPage.removeButton.click();
 
         //4. wait until "loading bar disappears"
+       /*
         Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
         wait.until(ExpectedConditions.invisibilityOf(dynamicControlsPage.loadingBar));
-
+       */
+        BrowserUtils.waitForInvisibilityOf(dynamicControlsPage.loadingBar);
 
 
         //5. verify:
         //a. checkbox is not displayed
         try {
+            Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
             Assert.assertTrue(!dynamicControlsPage.checkbox.isDisplayed());
             //assertFalse method will pass the test if the boolean value return is: false
             Assert.assertFalse(dynamicControlsPage.checkbox.isDisplayed());
